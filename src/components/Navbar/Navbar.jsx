b86cde7b70c5c1899ed57dae8c1bb4a05e296141
nathidaum <daumnathi@gmail.com>
2024-11-21 14:50:58 +0100

    feat(navbar): Implement links to favorites and bookings in desktop navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/auth.context";
-import { Menu, Avatar, Group, ActionIcon } from "@mantine/core";
+import { Menu, Avatar, Group } from "@mantine/core";
 import {
   IconSettings,
   IconLogout,
@@ -25,84 +25,74 @@ const Navbar = () => {
           spots.
         </Link>
 
-        {/* Profile section */}
-        <div className="profile-menu">
-          {isLoggedIn ? (
-            <Menu
-              shadow="lg"
-              width={180}
-            >
-              <Menu.Target>
-                <Group spacing="sm">
-                  <Avatar
-                    radius="xl"
-                    color="yellow"
-                    src={user?.avatar || undefined}
-                    alt={user?.firstName || "User"}
+        <div className="navbar-right">
+          {/* Navigation links */}
+          <div className="navbar-links">
+            <Link to="/favorites">Favorites</Link>
+            <Link to="/bookings">Bookings</Link>
+          </div>
+
+          {/* Profile menu */}
+          <div className="profile-menu">
+            {isLoggedIn ? (
+              <Menu shadow="lg" width={180}>
+                <Menu.Target>
+                  <div className="profile-box">
+                    <Avatar
+                      radius="xl"
+                      color="yellow"
+                      src={user?.avatar || undefined}
+                      alt={user?.firstName || "User"}
+                    >
+                      {user?.firstName && user?.lastName
+                        ? `${user.firstName[0]}${user.lastName[0]}`
+                        : "👋"}
+                    </Avatar>
+                    <span className="username">Hi, {user?.firstName}!</span>
+                  </div>
+                </Menu.Target>
+                <Menu.Dropdown>
+                  <Menu.Item
+                    component={Link}
+                    to="/profile/edit"
+                    icon={<IconSettings />}
                   >
-                    {/* Display initials if avatar isn't available */}
-                    {user?.firstName && user?.lastName
-                      ? `${user.firstName[0]}${user.lastName[0]}`
-                      : "👋"}
-                  </Avatar>
-                  <span className="username"> Hi, {`${user?.firstName}`}!</span>
-                </Group>
-              </Menu.Target>
-              <Menu.Dropdown>
-                <Menu.Item
-                  component={Link}
-                  to="/profile/edit"
-                  leftSection={
-                    <IconSettings style={{ width: 20, height: 20 }} />
-                  }
-                >
-                  Settings
-                </Menu.Item>
-                <Menu.Item
-                  leftSection={<IconLogout style={{ width: 20, height: 20 }} />}
-                  onClick={logOutUser}
-                >
-                  Logout
-                </Menu.Item>
-              </Menu.Dropdown>
-            </Menu>
-          ) : (
-            <Menu
-              shadow="lg"
-              width={180}
-            >
-              <Menu.Target>
-                <Group spacing="sm">
-                  <Avatar
-                    radius="xl"
-                    color="yellow"
-                    alt="no image here"
+                    Settings
+                  </Menu.Item>
+                  <Menu.Item icon={<IconLogout />} onClick={logOutUser}>
+                    Logout
+                  </Menu.Item>
+                </Menu.Dropdown>
+              </Menu>
+            ) : (
+              <Menu shadow="lg" width={180}>
+                <Menu.Target>
+                  <div className="profile-box">
+                    <Avatar radius="xl" color="yellow" alt="Guest">
+                      👋
+                    </Avatar>
+                    <span className="username">Hi there!</span>
+                  </div>
+                </Menu.Target>
+                <Menu.Dropdown>
+                  <Menu.Item
+                    component={Link}
+                    to="/login"
+                    icon={<IconLogin />}
                   >
-                    👋
-                  </Avatar>
-                  <span className="username"> Hi there!</span>
-                </Group>
-              </Menu.Target>
-              <Menu.Dropdown>
-                <Menu.Item
-                  component={Link}
-                  to="/login"
-                  leftSection={
-                    <IconLogin style={{ width: 20, height: 20 }} />
-                  }
-                >
-                  Login
-                </Menu.Item>
-                <Menu.Item
-                  component={Link}
-                  to="/register"
-                  leftSection={<IconUser style={{ width: 20, height: 20 }} />}
-                >
-                  Signup
-                </Menu.Item>
-              </Menu.Dropdown>
-            </Menu>
-          )}
+                    Login
+                  </Menu.Item>
+                  <Menu.Item
+                    component={Link}
+                    to="/register"
+                    icon={<IconUser />}
+                  >
+                    Signup
+                  </Menu.Item>
+                </Menu.Dropdown>
+              </Menu>
+            )}
+          </div>
         </div>
       </div>
 
