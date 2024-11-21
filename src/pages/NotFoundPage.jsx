f529eeb240cebda0601be67070e5e4c695a8bcc1
nathidaum <daumnathi@gmail.com>
2024-11-21 19:48:53 +0100

    feat(notfoundpage): style notfoundpage and link explore page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,13 +1,23 @@
 import { Link } from "react-router-dom";
+import { Button, Container, Group, Text, Title } from '@mantine/core';
+import "./notfoundpage.css"
 
 function NotFoundPage() {
   return (
     <div>
-      <h1>Ooooops!</h1>
-      <p>Here's nothing exctiting to see.</p>
-      <div>
-        <Link to="/">Explore spots</Link>
-      </div>
+    <Container className="notfoundpage">
+      <div className="notfoundlabel">404</div>
+      <Title className="notfoundtitle">Oh! You've found a secret spot here.</Title>
+      <Text c="dimmed" size="lg" ta="center" className="notfounddescription">
+        Let us show you where to find the real spots. 🤙
+      </Text>
+      <Group justify="center">
+        <Button variant="filled" size="md" color="yellow" component={Link}
+            to="/">
+          Take me to the spots
+        </Button>
+      </Group>
+    </Container>
     </div>
   );
 }
