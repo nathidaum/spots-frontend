86fd5b73900b9ee7e653007b5514838afcfb4f14
nathidaum <daumnathi@gmail.com>
2024-11-28 20:12:34 +0100

    fix(skeleton): improve skeleton for desktop

--- src/components/PageSkeleton.jsx
+++ src/components/PageSkeleton.jsx
@@ -1,46 +1,49 @@
-import { Container, Skeleton} from "@mantine/core";
+import { Skeleton} from "@mantine/core";
+import "../pages/explorepage.css"
 
 function PageSkeleton() {
-    return (
-        <div>
-        {Array.from({ length: 4 }).map((_, index) => (
-            <Container key={index} px={0} style={{ margin: 0 }}>
-              <Skeleton
-                height={240}
-                radius="lg"
-                mb="lg"
-                style={{
-                  width: "100%",
-                }}
-              />
-              <Skeleton
-                height={15}
-                radius="md"
-                mb="sm"
-                style={{
-                  width: "80%",
-                }}
-              />
-              <Skeleton
-                height={10}
-                radius="md"
-                mb="sm"
-                style={{
-                  width: "70%",
-                }}
-              />
-              <Skeleton
-                height={10}
-                radius="md"
-                mb="sm"
-                style={{
-                  width: "20%",
-                }}
-              />
-            </Container>
-          ))}
+  return (
+    <div className="gallery-container">
+      <div className="spotslist">
+        {Array.from({ length: 8 }).map((_, index) => (
+          <div key={index}>
+            <Skeleton
+              height={240}
+              radius="lg"
+              mb="lg"
+              style={{
+                width: "100%",
+              }}
+            />
+            <Skeleton
+              height={15}
+              radius="md"
+              mb="sm"
+              style={{
+                width: "80%",
+              }}
+            />
+            <Skeleton
+              height={10}
+              radius="md"
+              mb="sm"
+              style={{
+                width: "70%",
+              }}
+            />
+            <Skeleton
+              height={10}
+              radius="md"
+              mb="sm"
+              style={{
+                width: "20%",
+              }}
+            />
           </div>
-    )
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default PageSkeleton;
